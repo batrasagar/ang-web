a77302574ad56aefb8763629f42a3793ad8e8aeb
Batra Sagar <batrasagar@jet66erp.local>
2020-10-19 08:43:01 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BlogSingleComponent } from './blog-single/blog-single.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioDetailsComponent } from './portfolio-details/portfolio-details.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ServicesComponent } from './services/services.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: HomeComponent},
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path: 'services', component: ServicesComponent },
+  {path: 'portfolio', component: PortfolioComponent },
+  {path: 'portfolio-details', component: PortfolioDetailsComponent },
+  {path: 'pricing', component: PricingComponent },
+  {path: 'blog', component: BlogComponent },
+  {path: 'contact', component: ContactComponent },
+  {path: 'testimonials', component: TestimonialsComponent },
+  {path: 'blog-single', component: BlogSingleComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogSingleComponent } from './blog-single/blog-single.component';
+import { PortfolioDetailsComponent } from './portfolio-details/portfolio-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    TestimonialsComponent,
+    PricingComponent,
+    BlogComponent,
+    ContactComponent,
+    BlogSingleComponent,
+    PortfolioDetailsComponent
   ],
   imports: [
     BrowserModule,
